011da1c509eaf4360a4f9930482b5c9626d61186
Yoel Amadeo <yamadeo9@gmail.com>
2025-09-16 21:16:48 +0700

    Update train.py

--- src/train.py
+++ src/train.py
@@ -3,7 +3,7 @@ import mlflow.sklearn
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 import pandas as pd
 
 # Load dataset dari CSV (versi DVC)
@@ -22,6 +22,24 @@ with mlflow.start_run():
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
 
+    # Hitung berbagai metrik
     acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred, average='macro')
+    rec = recall_score(y_test, y_pred, average='macro')
+    f1 = f1_score(y_test, y_pred, average='macro')
+    cm = confusion_matrix(y_test, y_pred).tolist()  # convert ke list agar bisa log JSON
+
+    # Log metrik ke MLflow
     mlflow.log_metric("accuracy", acc)
-    print("Accuracy:", acc)
+    mlflow.log_metric("precision", prec)
+    mlflow.log_metric("recall", rec)
+    mlflow.log_metric("f1_score", f1)
+    mlflow.log_dict({"confusion_matrix": cm}, "confusion_matrix.json")
+
+    # Tampilkan di console
+    print(f"Accuracy: {acc:.4f}")
+    print(f"Precision: {prec:.4f}")
+    print(f"Recall: {rec:.4f}")
+    print(f"F1 Score: {f1:.4f}")
+    print("Confusion Matrix:")
+    print(cm)
