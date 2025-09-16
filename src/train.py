4f6767c97a07f3a36153057c8d8b082647b9f5f0
Yoel Amadeo <lensa.lui@gmail.com>
2025-09-16 18:13:21 +0700

    Setup MLflow + DVC + CI/CD workflow

---
+++ src/train.py
@@ -0,0 +1,27 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import pandas as pd
+
+# Load dataset dari CSV (versi DVC)
+df = pd.read_csv("data/iris.csv")
+X = df.drop("target", axis=1)
+y = df["target"]
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Aktifkan autolog MLflow
+mlflow.sklearn.autolog()
+
+with mlflow.start_run():
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+
+    acc = accuracy_score(y_test, y_pred)
+    mlflow.log_metric("accuracy", acc)
+    print("Accuracy:", acc)
